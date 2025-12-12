ca0c801f345483c47bc8ec582a9ce3c02f6a8013
Froylan <froylan@poball.com.mx>
2025-12-11 21:31:29 -0600

    finished

--- exercise2/exercise.sh
+++ exercise2/exercise.sh
@@ -1,16 +1,17 @@
 
 mkdir -p "./logs"
 
-touch "./logs/log.log"
-
+touch "./logs/log.txt"
 
 while true; do
     USE_RAM=$(free | awk 'NR==2 {printf "%.0f", $3/$2 * 100}')
     
-    if [ "$USE_RAM" -gt 70 ]; then
+    if [ "$USE_RAM" -gt 10 ]; then
         FILE_LOG="./log.txt"
         echo "Fecha: $(date)" > "$FILE_LOG"
         echo "Uso RAM: $USE_RAM%" >> "$FILE_LOG"
+        echo "" >> "$FILE_LOG"
+        echo "Procesos:" >> "$FILE_LOG"
         ps aux --sort=-%mem | head -6 >> "$FILE_LOG"
         
         echo "Log creado: $FILE_LOG"
